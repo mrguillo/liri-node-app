3165ed96a40ca1bae51798636ad0c09ed06c69b2
Memo Salcedo <guillosc@gmail.com>
2019-11-10 15:58:54 -0600

    concert-this, spotify and OMDB completed

--- liri.js
+++ liri.js
@@ -9,8 +9,8 @@ require("dotenv").config();
     var liriArgument = process.argv[2];
     var userINPUT = process.argv[3];
 
-    // var moment = require('moment');
-    // moment().format();
+    var moment = require('moment');
+    moment().format();
 
 
 //=================================================================
@@ -29,6 +29,58 @@ require("dotenv").config();
 			"\n OPTION: 4. do-what-it-says."+ "\n\n" +
 			"\n**********\nBe sure to put the movie or song name in quotation \nmarks if it's more than one word.\n**********\n\n\n");
     }
+
+
+// =================================================================
+	// concert-this <artist/band name here> function
+
+	//BANDS IN TOWN
+
+	function concertThis(){
+		var artist = userINPUT;
+		var space = "\n" + "\n" +"\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0";
+		console.log(space);
+		if (artist === undefined) {
+			artist = "Celine Dion"
+			console.log("Liri is executing concert-this for " + artist + " ...");
+		} 
+
+		axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+		.then(function(response) {
+			console.log('UP TO THIS POINT')
+			// console.log(response)
+			response.data.forEach(concert => {
+				// console.log(space);
+				console.log("---------------------------------------------------");
+				console.log(concert.venue.name)
+				console.log(concert.venue.city + ", " + (concert.venue.region || concert.venue.country))
+				console.log(moment(concert.datetime).format("MM/DD/YYYY"));
+				})
+				console.log("\n====== LIRI ====== LIRI ====== LIRI ====== LIRI====== LIRI ====== LIRI ======" + "\n" + "\n");
+		})
+		.catch(function(error) {
+			if (error.response) {
+			  // The request was made and the server responded with a status code
+			  // that falls out of the range of 2xx
+			  console.log("---------------Data---------------");
+			  console.log(error.response.data);
+			  console.log("---------------Status---------------");
+			  console.log(error.response.status);
+			  console.log("---------------Status---------------");
+			  console.log(error.response.headers);
+			} else if (error.request) {
+			  // The request was made but no response was received
+			  // `error.request` is an object that comes back with details pertaining to the error that occurred.
+			  console.log(error.request);
+			} else {
+			  // Something happened in setting up the request that triggered an Error
+			  console.log("Error", error.message);
+			}
+			console.log(error.config);
+		  });
+	}
+
+			
 
 // =================================================================
 	// Spotify function, Node Spotify API
